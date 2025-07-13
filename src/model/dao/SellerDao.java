71ef13ced065f06aa26f3bc54f505e1461f9adf3
Gustavo Marin <gustavosmsg18@gmail.com>
2025-07-13 15:54:39 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,6 +5,8 @@ import java.util.List;
 import model.entities.Seller;
 
 public interface SellerDao {
+	
+	// esse void Ou seja, ele faz alguma ação, mas não devolve nada para quem chamou.
 
 	void insert(Seller obj); //responsavel por inserir no banco de dados esse objeto que inseri
 	void update(Seller obj); //responsavel por atualizar no banco de dados esse objeto que inseri
