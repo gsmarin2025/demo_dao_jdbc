90a8a4b4fa3b47ed9d3a03c726fcec69bfd1c303
Gustavo Marin <gustavosmsg18@gmail.com>
2025-07-29 21:01:59 -0300

    delete implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -11,7 +11,7 @@ public interface SellerDao {
 
 	void insert(Seller obj); //responsavel por inserir no banco de dados esse objeto que inseri
 	void update(Seller obj); //responsavel por atualizar no banco de dados esse objeto que inseri
-	void deleteById(Seller obj); //responsavel por deletarPorId no banco de dados esse objeto que inseri
+	void deleteById(Integer id); //responsavel por deletarPorId no banco de dados esse objeto que inseri
 	Seller findById(Integer id); //Pega o Id e consultar no banco de dados um objeto com esse Id
 	List<Seller> findAll(); //retorna todos os Seller
 	List<Seller> findByDepartment(Department department); //buscar por departamento
